d73aed8b99fb45c424b2ef1268df0bc84925d707
Ruizheng <ruizhengxu@gmail.com>
2019-05-06 23:54:01 +0200

    Ajout de classe Sprite

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,12 +1,7 @@
 package fr.unilim.iut.spaceinvaders;
 
-public class Vaisseau {
+public class Vaisseau extends Sprite {
 
-	private Position origine;
-	private Dimension dimension;
-	private int vitesse;
-	
-	
 	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
 		this.dimension = dimension;
 		this.origine = positionOrigine;
@@ -25,55 +20,6 @@ public class Vaisseau {
 	
 	public Vaisseau(int longueur, int hauteur) {
 		this(longueur, hauteur, 0, 0);
-	}
-	
-	public Dimension getDimension() {
-		return this.dimension;
-	}
-	
-	public boolean occupeLaPosition(int x, int y) {
-		return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
-	}
-
-	private boolean estOrdonneeCouverte(int y) {
-		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-	}
-
-	public int ordonneeLaPlusHaute() {
-		return this.origine.ordonnee();
-	}
-
-	public int ordonneeLaPlusBasse() {
-		return ordonneeLaPlusHaute() - this.dimension.hauteur()+1;
-	}
-
-	private boolean estAbscisseCouverte(int x) {
-		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-	}
-
-	public int abscisseLaPlusADroite() {
-		return this.origine.abscisse() + this.dimension.longueur() - 1;
-	}
-	
-
-	public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	}
-
-	
-	public int abscisseLaPlusAGauche() {
-		return this.origine.abscisse();
-	}
-
-
-	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	}
-
-
-	public void positionner(int x, int y) {
-		this.origine.changerAbscisse(x);
-	    this.origine.changerOrdonnee(y);
 	}
 	
 	
