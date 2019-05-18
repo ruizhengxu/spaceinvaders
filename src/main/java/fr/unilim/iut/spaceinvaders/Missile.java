13abccc88c914f63bba808dc6863a6424e37a740
Ruizheng <ruizhengxu@gmail.com>
2019-05-18 11:27:44 +0200

    Création classe MissileException
    Création claase MissileTest

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Missile.java
@@ -0,0 +1,8 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class Missile extends Sprite {
+
+	public Missile(Dimension dimensionMissile, Position positionMissile, int vitesseMissile) {
+		super(dimensionMissile, positionMissile, vitesseMissile);
+	}
+}
