148ecb68d7b09532f44c56890b539f876b89f8ab
Ruizheng <ruizhengxu@gmail.com>
2019-05-29 15:10:51 +0200

    Collision

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
@@ -4,6 +4,11 @@ public class Collision {
 	
 	public boolean detecterCollision(Missile missile, Envahisseur envahisseur) {
 		return(this.collisionAbscisse(missile, envahisseur) && this.collisionOrdonnee(missile, envahisseur));
+		/*return ((this.collisionAbscisseDroite(missile, envahisseur) || 
+				this.collisionAbscisseGauche(missile, envahisseur)) &&
+				(this.collisionOrdonneeEnvahisseur(missile, envahisseur) ||
+						this.collisionOrdonneeBasse(missile, envahisseur) ||
+						this.collisionOrdonneeHaute(missile, envahisseur)) );*/
 	}
 	
 	private boolean collisionAbscisse(Missile missile, Envahisseur envahisseur) {
@@ -14,6 +19,31 @@ public class Collision {
 	private boolean collisionOrdonnee(Missile missile, Envahisseur envahisseur) {
 		return(missile.ordonneeLaPlusBasse() <= envahisseur.ordonneeLaPlusHaute() &&
 				missile.ordonneeLaPlusHaute() >= envahisseur.ordonneeLaPlusBasse());
+	}
+	
+	private boolean collisionOrdonneeEnvahisseur(Missile missile, Envahisseur envahisseur) {
+		return(envahisseur.ordonneeLaPlusHaute() <= missile.ordonneeLaPlusHaute() &&
+				envahisseur.ordonneeLaPlusBasse() >= missile.ordonneeLaPlusBasse());
+	}
+
+	private boolean collisionAbscisseGauche(Missile missile, Envahisseur envahisseur) {
+		return(missile.abscisseLaPlusAGauche() >= envahisseur.abscisseLaPlusAGauche()
+				&& missile.abscisseLaPlusAGauche() <= envahisseur.abscisseLaPlusADroite());
+	}
+	
+	private boolean collisionAbscisseDroite(Missile missile, Envahisseur envahisseur) {
+		return(missile.abscisseLaPlusADroite() <= envahisseur.abscisseLaPlusADroite()
+				&& missile.abscisseLaPlusADroite() >= envahisseur.abscisseLaPlusAGauche());
+	}
+	
+	private boolean collisionOrdonneeBasse(Missile missile, Envahisseur envahisseur) {
+		return(missile.ordonneeLaPlusBasse() >= envahisseur.ordonneeLaPlusBasse() &&
+				missile.ordonneeLaPlusBasse() <= envahisseur.ordonneeLaPlusHaute());
+	}
+	
+	private boolean collisionOrdonneeHaute(Missile missile, Envahisseur envahisseur) {
+		return(missile.ordonneeLaPlusHaute() >= envahisseur.ordonneeLaPlusBasse() &&
+				missile.ordonneeLaPlusHaute() <= envahisseur.ordonneeLaPlusHaute());
 	}
 	
 }
